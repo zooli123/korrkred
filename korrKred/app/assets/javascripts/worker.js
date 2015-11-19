6404ee749f3d2098f7f81fe81fc24ec9deeeadb9
Zoltán János Szabó <123zooli@gmail.com>
2015-11-19 21:17:19 +0100

    More stylefix and change.

--- korrKred/app/assets/javascripts/worker.js
+++ korrKred/app/assets/javascripts/worker.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-  $("#add_new_semester_button").on("click", function(){
-    $(".new_semester_div").css("display", "initial");
-    $("#save_semester_button").css("display", "initial");
-    $("#add_new_semester_button").css("display", "none");
-  })
-
-  $("#add_new_subject_button").on("click", function(){
-    $(".new_subject_div").css("display", "initial");
-    $(".paste_subject_div").css("display", "initial");
-    $("#save_subject_button").css("display", "initial");
-    $("#add_subject_button").css("display", "initial");
-    $("#add_new_subject_button").css("display", "none");
-  })
 
   $("input[name=delete_subject]").on("click", function(e){
     confirmation(e);
