a74e65d6d0742fb0b321c1514b29a98819c03a0d
Zoltán János Szabó <123zooli@gmail.com>
2015-11-19 01:45:26 +0100

    Changed style to bootstrap.

--- korrKred/app/assets/javascripts/worker.js
+++ korrKred/app/assets/javascripts/worker.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
   $("#add_new_semester_button").on("click", function(){
     $(".new_semester_div").css("display", "initial");
     $("#save_semester_button").css("display", "initial");
+    $("#add_new_semester_button").css("display", "none");
   })
+
   $("#add_new_subject_button").on("click", function(){
     $(".new_subject_div").css("display", "initial");
     $(".paste_subject_div").css("display", "initial");
     $("#save_subject_button").css("display", "initial");
     $("#add_subject_button").css("display", "initial");
+    $("#add_new_subject_button").css("display", "none");
   })
 
   $("input[name=delete_subject]").on("click", function(e){
@@ -16,6 +19,8 @@ $(document).ready(function(){
   $("input[name=delete_subject_from_semester]").on("click", function(e){
     confirmation(e);
   })
+
+  set_active_page();
 
   $(".credit_index").on("mouseover", function(){
     var message = gon.label_withdrawn + ": " + gon.withdrawn_credits + "\n" + gon.label_accomplished + ": " + gon.accomplished_credits;
@@ -28,5 +33,27 @@ function confirmation(e){
   if (confirmation == true) {
   } else {
     e.preventDefault();
+  }
+}
+
+function recognize_page(path){
+  var match = /semesters/.test(path);
+  var match2 = /semesters\/[0-9]+/.test(path);
+  if (match && !match2)
+    return "semester";
+  match = /subjects/.test(path);
+  if (match)
+    return "subject"
+  return "none"
+}
+
+function set_active_page () {
+  if (recognize_page(window.location.pathname) == "semester")
+    $("#nav_button_semesters").addClass("active");
+  else if (recognize_page(window.location.pathname) == "subject")
+    $("#nav_button_subjects").addClass("active");
+  else{
+    $("#nav_button_subjects").removeClass("active");
+    $("#nav_button_semesters").removeClass("active");
   }
 }
